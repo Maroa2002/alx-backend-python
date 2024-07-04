19252027f17f101f9fae3433ad433c6f02b288de
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-04 08:15:58 +0300

    type-annotated function floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""type-annotated function floor"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+        args:
+            n: float
+        return: float
+    """
+    return math.floor(n)
