1dbbda4ca132126df29af3fced8afe425fc31893
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-04 08:22:47 +0300

    type-annotated function to_str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""type-annotated function floor"""
+
+
+def to_str(n: float) -> str:
+    """
+        args:
+            n: float
+        return: str
+    """
+    return str(n)
