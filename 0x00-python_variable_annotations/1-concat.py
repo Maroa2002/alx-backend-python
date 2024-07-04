d881be31f485c85fb761817e6424ab1284a6f731
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-04 08:05:00 +0300

    basic annotations to function concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        args:
+            str1: str
+            str2: str
+        return: str
+    """
+    return str1 + str2
